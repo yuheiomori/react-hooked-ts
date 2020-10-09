6fcc40364bd86012e741690c457a3074341cad1f
Yuhei Omori <omori.yuhei@panair.jp>
2020-10-09 20:06:16 +0900

    useReducer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,46 +1,109 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useEffect , Reducer} from 'react';
 import '../App.css';
 import Header from "./Header";
 import Movie, { IMovie } from "./Movie";
 import Search from "./Search";
 
-console.log(process.env);
+
 const INITIAL_SEARCH_KEY = "man"
 const OMDB_API_KEY = process.env.REACT_APP_OMDB_API_KEY;
 const MOVIE_API_URL = `https://www.omdbapi.com/?s=${INITIAL_SEARCH_KEY}&apikey=${OMDB_API_KEY}`; 
 
+enum ActionType {
+  SEARCH_MOVIES_REQUEST = "SEARCH_MOVIES_REQUEST",
+  SEARCH_MOVIES_SUCCESS = "SEARCH_MOVIES_SUCCESS",
+  SEARCH_MOVIES_FAILURE = "SEARCH_MOVIES_FAILURE"
+}
+
+interface IAppState {
+  loading: boolean
+  movies: IMovie[]
+  errorMessage?: string | null
+}
+
+interface IAppAction {
+  type: ActionType,
+  payload?: IAppState["movies"]
+  error?: IAppState["errorMessage"]
+}
+
+
+const initialState: IAppState = {
+  loading: true,
+  movies: [],
+  errorMessage: null
+};
+
+
+const reducer = (state:  IAppState, action: IAppAction): IAppState => {
+  switch (action.type) {
+    case ActionType.SEARCH_MOVIES_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null
+      };
+    case ActionType.SEARCH_MOVIES_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        // !でエラー回避
+        movies: action.payload!
+      };
+    case ActionType.SEARCH_MOVIES_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error!
+      };
+    default:
+      return state;
+  }
+};
+
 
 
 const App = () => {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [movies, setMovies] = useState([]);
-  const [errorMessage, setErrorMessage] = useState(null);
+
+  const [state, dispatch] = useReducer<Reducer<IAppState, IAppAction>>(reducer, initialState)
+
+  
 
   useEffect(() => {
     fetch(MOVIE_API_URL)
       .then(response => response.json())
       .then(jsonResponse => {
-        setMovies(jsonResponse.Search);
-        setLoading(false);
+        dispatch({
+          type: ActionType.SEARCH_MOVIES_SUCCESS,
+          payload: jsonResponse.Search
+        })
       });
   }, []);
 
   const search = (searchValue: string) => {
-    setLoading(true);
-    setErrorMessage(null);
+    dispatch({
+      type: ActionType.SEARCH_MOVIES_REQUEST,
+    })
 
     fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=${OMDB_API_KEY}`)
       .then(response => response.json())
       .then(jsonResponse => {
         if (jsonResponse.Response === "True") {
-          setMovies(jsonResponse.Search);
-          setLoading(false);
+          dispatch({
+            type: ActionType.SEARCH_MOVIES_SUCCESS,
+            payload: jsonResponse.Search
+          })
+
         } else {
-          setErrorMessage(jsonResponse.Error);
-          setLoading(false);
+          dispatch({
+            type: ActionType.SEARCH_MOVIES_FAILURE,
+            error: jsonResponse.Error
+          })
         }
       });
   };
+
+  const {movies, errorMessage, loading} = state;
 
 
   return (
